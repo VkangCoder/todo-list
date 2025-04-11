7516513c9ec570c61beeb975db91e6eb5a4e9d56
Khang000 <khangquach026@gmail.com>
2025-04-11 15:32:15 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Hello world</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
